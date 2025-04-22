d82fa0fca65d34c15b07fcc8a148e4db4b61af29
paulinho.borges <paulinhodeveloper@outlook.com>
2025-04-22 16:16:18 -0300

    Criado o projeto com spring-initializr

---
+++ src/main/java/com/projeto/pokemonapi/PokemonapiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.pokemonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonapiApplication.class, args);
+	}
+
+}
